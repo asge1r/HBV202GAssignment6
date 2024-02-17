f02334e6873165273e55e1f0e7e7e94d2c66d98a
Helmut Neukirchen <helmut@hi.is>
2024-02-17 09:49:50 +0000

    Update CarTest.java: white space

--- src/test/java/is/hi/hbv202g/ass6/CarTest.java
+++ src/test/java/is/hi/hbv202g/ass6/CarTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class CarTest {
     @Test
     public void getNameOfCar() {
-        Car car=new Car("Beetle");
+        Car car = new Car("Beetle");
         CarOwner carOwner = new CarOwner("John Doe", car);
         assertEquals("Beetle", carOwner.getNameOfCar());
     }
